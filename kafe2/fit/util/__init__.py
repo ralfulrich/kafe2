d195c3a1467c09cc90efe3e9e8ccfcb3256d42ff
Daniel Savoiu <daniel.savoiu@cern.ch>
2019-10-08 10:46:47 +0200

    [util] New utility functions `maybe_invert_matrix` and dict iterators.

--- kafe2/fit/util/__init__.py
+++ kafe2/fit/util/__init__.py
@@ -6,7 +6,9 @@ r"""
 .. moduleauthor:: Johannes Gaessler <johannes.gaessler@student.kit.edu>
 """
 
-import numpy as np
+import numpy as _np
+from functools import partial as _partial
+import itertools as _itertools
 
 from . import function_library
 
@@ -21,19 +23,39 @@ def string_join_if(pieces, delim='_', condition=lambda x: x):
     return delim.join((p for p in pieces if condition(p)))
 
 
+def _iter_dict(dict_, iter_func, **iter_func_kwargs):
+    """Yield sequence of dicts whose keys map to the results of applying `iter_func` on the values"""
+    _keys = dict_.keys()
+    for instance in iter_func(*dict_.values(), **iter_func_kwargs):
+        yield dict(zip(_keys, instance))
+
+
+# some concrete
+product_dict = _partial(_iter_dict, iter_func=_itertools.product)
+zip_dict = _partial(_iter_dict, iter_func=zip)
+zip_longest_dict = _partial(_iter_dict, iter_func=_itertools.zip_longest)
+
+
 # -- array/matrix utility functions
 
 def add_in_quadrature(*args):
     '''return the square root of the sum of squares of all arguments'''
-    return np.sqrt(np.sum([_a**2 for _a in args], axis=0))
+    return _np.sqrt(_np.sum([_a**2 for _a in args], axis=0))
+
 
 def invert_matrix(mat):
     '''perform matrix inversion'''
-    #try:
-    return np.linalg.inv(mat)
-    #except np.linalg.LinAlgError:
-    #    return None
+    return _np.linalg.inv(mat)
+
+
+def maybe_invert_matrix(mat):
+    '''perform matrix inversion and return None if not possible'''
+    try:
+        return invert_matrix(mat)
+    except _np.linalg.LinAlgError:
+        return None
+
 
 def collect(*args):
     '''collect arguments into array'''
-    return np.asarray(args)
+    return _np.asarray(args)
